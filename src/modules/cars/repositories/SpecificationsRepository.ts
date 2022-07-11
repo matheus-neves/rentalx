3cd66c0fc76bb47475926e058bd34bf3720fdeed
Matheus Neves <matheusneves1989@gmail.com>
2022-07-11 11:21:19 -0300

    feat(Specification): create specifications repository

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,33 @@
+import { Specification } from "../model/Specification";
+import { ICreateCategoryDTO } from "./ICategoriesRepository";
+import { ISpecificationsRepository } from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ description, name }: ICreateCategoryDTO) {
+    const specification = new Specification();
+
+    const newSpecification: Specification = {
+      ...specification,
+      name,
+      description,
+      created_at: new Date(),
+    };
+
+    this.specifications.push(newSpecification);
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+    return specification;
+  }
+}
+
+export { SpecificationsRepository };
